f655a7b1952d3402aaa4735874c8107f61ef8562
Eiruddy <114981230+Eiruddy@users.noreply.github.com>
2024-05-09 21:15:31 +0800

    Add files via upload

--- dag.go
+++ dag.go
@@ -1,41 +1,35 @@
 package merkledag
 
-import "hash"
+import (
+	"bytes"
+	"hash"
+	"hash/fnv"
+)
 
-type Link struct {
-	Name string
-	Hash []byte
-	Size int
-}
-
-type Object struct {
-	Links []Link
-	Data  []byte
-}
-
-// Add 将一个节点添加到 Merkle DAG 中，并返回 Merkle Root。
+// Add 将 Node 中的数据保存在 KVStore 中，然后计算出 Merkle Root
 func Add(store KVStore, node Node, hp HashPool) []byte {
-    // 检查节点是否是文件
-    if fileNode, ok := node.(File); ok {
-        // 如果是文件，则计算其哈希值并将数据存储到 KVStore 中
-        data := fileNode.Bytes()
-        hash := calculateHash(data)
-        store.Put(hash, data)
-        return hash
-    } else if dirNode, ok := node.(Dir); ok {
-        // 如果是文件夹，则递归地添加其子节点
-        var childHashes [][]byte
-        it := dirNode.It()
-        for it.Next() {
-            childNode := it.Node()
-            childHash := Add(store, childNode, hp)
-            childHashes = append(childHashes, childHash)
-        }
-        // 根据子节点的哈希值计算文件夹节点的哈希值
-        dirHash := calculateHash([]byte(fmt.Sprintf("%v", childHashes)))
-        return dirHash
-    } else {
-        // 处理不支持的节点类型
-        return nil
-    }
+	rootHash := computeMerkleRoot(node, store, hp)
+	return rootHash
+}
+
+// computeMerkleRoot 计算 Merkle Root
+func computeMerkleRoot(node Node, store KVStore, hp HashPool) []byte {
+	switch n := node.(type) {
+	case *File:
+		// 对文件进行哈希计算
+		h := hp.Get()
+		h.Write(n.Bytes())
+		return h.Sum(nil)
+	case *Dir:
+		// 对文件夹中的每个文件或文件夹进行哈希计算
+		h := hp.Get()
+		for iter := n.It(); iter.Next(); {
+			childNode := iter.Node()
+			childHash := computeMerkleRoot(childNode, store, hp)
+			h.Write(childHash)
+		}
+		return h.Sum(nil)
+	default:
+		return nil
+	}
 }
--- dag2file.go
+++ dag2file.go
@@ -1,37 +1,11 @@
 package merkledag
 
-// Hash2File 根据哈希值和路径从 KVStore 中获取文件内容。
+// Hash2File 根据 hash 和 path 返回对应的文件内容
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-    // 从 KVStore 中根据哈希值获取节点数据
-    data := store.Get(hash)
-    if data == nil {
-        // 如果节点不存在，则返回空
-        return nil
-    }
-    
-    // 解析节点数据
-    obj := parseObject(data)
-    
-    // 如果路径为空，直接返回节点数据
-    if path == "" {
-        return obj.Data
-    }
-    
-    // 如果节点是文件夹，根据路径遍历子节点
-    for _, link := range obj.Links {
-        if link.Name == path {
-            // 找到对应的子节点，递归调用 Hash2File
-            return Hash2File(store, link.Hash, "", hp)
-        }
-    }
-    
-    // 如果路径不存在，返回空
-    return nil
-}
-
-// parseObject 解析节点数据
-func parseObject(data []byte) *Object {
-    // 解析数据并返回对象
-    // 这里根据具体情况进行解析，暂时使用占位符
-    return &Object{}
+	// 从 KVStore 中获取 hash 对应的数据
+	data, err := store.Get(hash)
+	if err != nil {
+		return nil
+	}
+	return data
 }
